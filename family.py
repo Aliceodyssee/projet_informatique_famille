d3351ba577e0f38478392a5bb60ce22e9b80762a
Olyem <olympe.tournier@outlook.fr>
2022-06-01 16:14:06 +0200

    family class

---
+++ family.py
@@ -0,0 +1,23 @@
+class family :
+    def __init__(self) :
+        self.id = None
+        self.parents = None
+        self.children = None
+    
+    def get_parents() :
+        """
+        Returns
+        ---
+        parents : two element list
+        0 : father
+        1 : moter
+        """
+        pass
+
+    def get_children() :
+        pass
+
+
+
+
+    
