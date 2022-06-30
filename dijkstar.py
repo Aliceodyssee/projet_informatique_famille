b0be26e32fcd8e01b2a2c13470662e30992348d0
Olyem <olympe.tournier@outlook.fr>
2022-06-30 14:54:21 +0200

    dernières modifs dijkstar

--- dijkstar.py
+++ dijkstar.py
@@ -8,10 +8,17 @@ import pandas as pd
 import math
 from dijkstar import Graph, find_path
 
-
 # Parser
 gedcom_parser = Parser()
 file_path = 'Queen_Eliz_II.ged'
 gedcom_parser = Parser()
 gedcom_parser.parse_file(file_path)
 root_child_elements = gedcom_parser.get_root_child_elements()
+
+# dijk_search(graph, v1,v2)
+
+# creation of Graph() object
+
+def make_graph_dijkstar(graph) :
+    new_graph = Graph()
+    for 
