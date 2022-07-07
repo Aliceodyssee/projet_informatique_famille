b2b3e9aa84a9204a5e40ad64313fd172182eb420
Aliceodyssee <belliard97@gmail.com>
2022-07-07 11:53:34 +0200

    commit visuel

--- visuel.py
+++ visuel.py
@@ -1,58 +1,84 @@
+import igraph
+from igraph import Graph, EdgeSeq
+nr_vertices = 25
+v_label = list(map(str, range(nr_vertices)))
+G = Graph.Tree(nr_vertices, 2) # 2 stands for children number
+lay = G.layout('rt')
 
-class Noeud:
-    def __init__(self, valeur):
-        self.valeur = valeur
-        self.enfant_gauche = None
-        self.enfant_droit = None
+position = {k: lay[k] for k in range(nr_vertices)}
+Y = [lay[k][1] for k in range(nr_vertices)]
+M = max(Y)
 
-    def insert_gauche(self, valeur):
-        if self.enfant_gauche == None:
-            self.enfant_gauche = Noeud(valeur)
-        else:
-            new_node = Noeud(valeur)
-            new_node.enfant_gauche = self.enfant_gauche
-            self.enfant_gauche = new_node
+es = EdgeSeq(G) # sequence of edges
+E = [e.tuple for e in G.es] # list of edges
 
-    def insert_droit(self, valeur):
-        if self.enfant_droit == None:
-            self.enfant_droit = Noeud(valeur)
-        else:
-            new_node = Noeud(valeur)
-            new_node.enfant_droit = self.enfant_droit
-            self.enfant_droit = new_node
-    def get_valeur(self):
-        return self.valeur
-    def get_gauche(self):
-        return self.enfant_gauche
-    def get_droit(self):
-        return self.enfant_droit
-    
-def affiche(arbre):
-    if arbre != None:
-        return (arbre.get_valeur(), affiche(arbre.get_gauche()), affiche(arbre.get_droit()))
+L = len(position)
+Xn = [position[k][0] for k in range(L)]
+Yn = [2*M-position[k][1] for k in range(L)]
+Xe = []
+Ye = []
+for edge in E:
+    Xe+=[position[edge[0]][0],position[edge[1]][0], None]
+    Ye+=[2*M-position[edge[0]][1],2*M-position[edge[1]][1], None]
 
-
-racine = Noeud('A')
-racine.insert_gauche('B')
-racine.insert_droit('F')
-
-b_node = racine.get_gauche()
-b_node.insert_gauche('C')
-b_node.insert_droit('D')
-
-f_node = racine.get_droit()
-f_node.insert_gauche('G')
-f_node.insert_droit('H')
-
-c_node = b_node.get_gauche()
-c_node.insert_droit('E')
-
-g_node = f_node.get_gauche()
-g_node.insert_gauche('I')
-
-h_node = f_node.get_droit()
-h_node.insert_droit('J')
-
+labels = v_label
    
-print(affiche(racine))
-   
+import plotly.graph_objects as go
+
+fig = go.Figure()
+fig.add_trace(go.Scatter(x=Xe,
+                   y=Ye,
+                   mode='lines',
+                   line=dict(color='rgb(210,210,210)', width=1),
+                   hoverinfo='none'
+
+                   ))
+fig.add_trace(go.Scatter(x=Xn,
+                  y=Yn,
+                  mode='markers',
+                  name='bla',
+                  marker=dict(symbol='circle-dot',
+                                size=18,
+                                color='#6175c1',    #'#DB4551',
+                                line=dict(color='rgb(50,50,50)', width=1)
+                                ),
+                  text=labels,
+                  hoverinfo='text',
+                  opacity=0.8
+                  ))
+
+def make_annotations(pos, text, font_size=10, font_color='rgb(250,250,250)'):
+    L=len(pos)
+    if len(text)!=L:
+        raise ValueError('The lists pos and text must have the same len')
+    annotations = []
+    for k in range(L):
+        annotations.append(
+            dict(
+                text=labels[k], # or replace labels with a different list for the text within the circle
+                x=pos[k][0], y=2*M-position[k][1],
+                xref='x1', yref='y1',
+                font=dict(color=font_color, size=font_size),
+                showarrow=False)
+        )
+    return annotations
+
+
+
+axis = dict(showline=False, # hide axis line, grid, ticklabels and  title
+            zeroline=False,
+            showgrid=False,
+            showticklabels=False,
+            )
+
+fig.update_layout(title= 'chemin généalogique',
+              annotations=make_annotations(position, v_label),
+              font_size=12,
+              showlegend=False,
+              xaxis=axis,
+              yaxis=axis,
+              margin=dict(l=40, r=40, b=85, t=100),
+              hovermode='closest',
+              plot_bgcolor='rgb(248,248,248)'
+              )
+fig.show()
